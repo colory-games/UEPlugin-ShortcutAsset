947cce5fe1c297ff590fa6393480608e080ba1fc
nutti <nutti.metro@gmail.com>
2023-03-17 22:54:01 +0900

    Add: License (#5)

--- ShortcutAsset/Source/ShortcutAsset/Private/ShortcutAssetFactoryNew.cpp
+++ ShortcutAsset/Source/ShortcutAsset/Private/ShortcutAssetFactoryNew.cpp
@@ -1,3 +1,12 @@
+/*!
+ * ShortcutAsset
+ *
+ * Copyright (c) 2023 Colory Games
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 #include "ShortcutAssetFactoryNew.h"
 
 #include "ShortcutAsset.h"
